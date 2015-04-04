cf66dd740ace186b3563a89a8e24eef594483fdc
Bob Schmertz <rschmertz@yahoo.com>
2015-04-04 14:47:10 -0400

    Created directive for file item.

---
+++ client/app/scripts/directives/fileItem.js
@@ -0,0 +1,10 @@
+angular.module('mentorApp')
+    .directive('fileItem', function () {
+	return {
+	    restrict:'E',
+	    templateUrl: 'fileItemTpl',
+	    link:function (scope, el, attrs, ctrl) {
+        
+	    }
+	};
+    });
