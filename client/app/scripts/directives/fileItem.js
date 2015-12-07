c8a8ea49d005f29653c2ec1ed3d23a8f65ca4396
Bob Schmertz <rschmertz@yahoo.com>
2015-12-06 20:11:27 -0500

    Broken, badly-performing rework to use trello API

--- client/app/scripts/directives/fileItem.js
+++ client/app/scripts/directives/fileItem.js
@@ -7,7 +7,7 @@ angular.module('mentorApp')
         
 	    }
 	};
-    }).controller("docListCtrl", function () {
+    }).controller("docListCtrl", ['$http', function ($http) {
         // We'll add some changes here
         this.icons = {
             doc: "img/file-icons/docx/docx_win-64_32.png",
@@ -15,18 +15,60 @@ angular.module('mentorApp')
             pdf: "img/file-icons/pdf/pdf-64_32.png"
         };
 
+        this.doclist1 = [];
+        var self = this;
+        // "Documents" list
+        $http.get('https://api.trello.com/1/lists/566397b374bba06e49402f2d/cards')
+            .success(function(data, status) {
+                console.log(data);
+                
+                data.forEach(function (card) {
+                    
+                    var cardThing = {
+                        name: card.name,
+                        url: card.url,
+                        desc: card.desc,
+                    }; 
+                    self.doclist1.push(cardThing);
+                    console.log("Stupid url", card.url);
+                    cardThing.attachment = $http.get('https://api.trello.com/1/cards/' + card.id + '/attachments')
+                        .success(function (data, status) {
+                            /*
+                            data.forEach(function(attachment) {
+                                console.log("Hey, it's at " + attachment.url);
+                                console.log("oh, the attachment is ", attachment);
+                                cardThing.attachment = attachment;
+                            });
+                            */
+                        })
+                        .error(function (data, status) {
+                            console.log("problem getting attachments");
+                        });
+                });
+                setTimeout(function () {
+                    console.log(self.doclist1);
+                }, 4000);
+            })
+            .error(function (data, status) {
+                console.log("Shoot, we couldn't get it to work");
+            });
+        
         var fileNameRegex = new RegExp("http.*box.com/s/[a-z0-9]+/(.+\.(pdf|doc|docx))\\?dl=.");
+        fileNameRegex = new RegExp(".+\.(pdf|doc|docx)");
         this.getFileName = function(url) {
             return decodeURI(url.match(fileNameRegex)[1]);
         };
         this.getIcon = function(url) {
+            console.log("url is ", url);
+            //return 'doc';
             var match = url.match(fileNameRegex);
-            return this.icons[match[2]];
+            console.log("matchi is ", match[1]);
+            return this.icons[match[1]];
         };
         this.adjustUrl = function(url) {
             return url.replace(/dl=0$/, "dl=1");
         };
-        this.doclist1 = [
+        this.vdoclist1 = [
             {
                 description: "Section Leaders",
                 url: "https://www.dropbox.com/s/3bx8wxe1ny3ttsp/Section%20Leaders.docx?dl=0"
@@ -40,7 +82,7 @@ angular.module('mentorApp')
                 url: "https://www.dropbox.com/s/gnaoj110ali4dg8/MDCB%20Concert%20Flyer%209.10.15-Composers-FINAL-Indent.pdf?dl=0"
             },
         ];
-        this.doclist2 = [
+        this.vdoclist2 = [
             {
                 description: "Band Librarian Committee",
                 url: "https://www.dropbox.com/s/hnwiyam9owvr7dl/BAND%20LIBRARIAN.docx?dl=0"
@@ -70,4 +112,4 @@ angular.module('mentorApp')
                 url: "https://www.dropbox.com/s/1iw069bxnww8ijk/WEBSITE.docx?dl=0"
             },
         ];
-    });
+    }]);
