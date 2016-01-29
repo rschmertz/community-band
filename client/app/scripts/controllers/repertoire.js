2047ea3b1b95a0c7236ff5200e54a0a1713158db
Bob Schmertz <rschmertz@yahoo.com>
2016-01-29 04:07:00 -0500

    Audio clips played from local FS

--- client/app/scripts/controllers/repertoire.js
+++ client/app/scripts/controllers/repertoire.js
@@ -4,7 +4,7 @@ angular.module('mentorApp.controllers.about', [])
   .controller('RepertoireCtrl', ['$scope', function ($scope) {
 
       $scope.adjustUrl = function(url) {
-          return url.replace(/dl=0$/, "dl=1");
+          return url.replace(/dl=0$/, "dl=0");
       };
 
       $scope.rep = [
@@ -13,15 +13,15 @@ angular.module('mentorApp.controllers.about', [])
               listing: [
                   {
                       name: "Concerto for Piccolo and Band – Vivaldi",
-                      link: "https://www.dropbox.com/s/fjv31a9a47p9jru/ConcertoForPiccoloAndBand_Vivaldi_PartI_04APR2012.mp3?dl=0"
+                      link: "audio/ConcertoForPiccoloAndBand_Vivaldi_PartI_04APR2012.mp3"
                   },{
                       name: "Prelude and Fugue in Ab – JS Bach",
                   },{
                       name: "Barber of Seville – G Rossini",
-                      link: "https://www.dropbox.com/s/2wnrwe7lohuyu86/BarberOfSeville_Rossini_30OCT2013.mp3?dl=0",
+                      link: "audio/BarberOfSeville_Rossini_30OCT2013.mp3",
                   },{
                       name: "Capriol Suite – P Warlock",
-                      link: "https://www.dropbox.com/s/qyc5g57dw5zvgmf/CapriolSuite-Mattachins_Warlock_12NOV2014.mp3?dl=0",
+                      link: "audio/CapriolSuite-Mattachins_Warlock_12NOV2014.mp3",
                   },{
                       name: "Symphony No. 3 Finale – G Mahler",
                   }
@@ -38,8 +38,8 @@ angular.module('mentorApp.controllers.about', [])
                   },{
                       name: "The Stars and Stripes Forever March – JP Sousa",
                   },{
-                      name: "Moorside March – G Holst",
-                      link: "https://www.dropbox.com/s/lpmwyuk6jxmzgd7/FirstSuiteEbMilitaryBand_Holst_02NOV2011.mp3?dl=0",
+                      name: "First Suite – G Holst",
+                      link: "audio/FirstSuiteEbMilitaryBand_Holst_02NOV2011.mp3",
                   }
               ]
           }, {
@@ -58,10 +58,10 @@ angular.module('mentorApp.controllers.about', [])
               listing: [
                   {
                       name: "Bugler’s Holiday – L Anderson",
-                      link: "https://www.dropbox.com/s/8xpvm2ymctzg958/BuglersHoliday_Anderson%28Head%2CGekker%2CAlmalgrom%2C%20Trumpets%29_09APR2011.mp3?dl=1",
+                      link: "audio/BuglersHoliday_Anderson%28Head%2CGekker%2CAlmalgrom%2C%20Trumpets%29_09APR2011.mp3",
                   },{
                       name: "Harry Potter Suite – J Williams",
-                      link: "https://www.dropbox.com/s/qf1y2rnfny7tik1/HarryPotterSymphonicSuite_Williams_12NOV2014.mp3?dl=0",
+                      link: "audio/HarryPotterSymphonicSuite_Williams_12NOV2014.mp3",
                   }
               ]
           }, {
@@ -69,7 +69,7 @@ angular.module('mentorApp.controllers.about', [])
               listing: [
                   {
                       name: "Summertime (Porgy & Bess) – G Gershwin (Millicent Scarlett, soprano)",
-                      link: "https://www.dropbox.com/s/yemi5t2z1invaoc/Summertime_GershwinMillicentScarlett%2CSoprano%2924June2011.mp3?dl=0",
+                      link: "audio/Summertime_GershwinMillicentScarlett%2CSoprano%2924June2011.mp3",
                   },{
                       name: "Seventy-Six Trombones – M Willson",
                   },{
@@ -86,7 +86,7 @@ angular.module('mentorApp.controllers.about', [])
                       name: "An Irish Rhapsody – C Grundman",
                     },{
                         name: "Loch Lomond – F Ticheli",
-                        link: "https://www.dropbox.com/s/v84r3g6y4kzcblc/LochLomond_Tichelli_02NOV2011.mp3?dl=0",
+                        link: "audio/LochLomond_Tichelli_02NOV2011.mp3",
                     },{
                         name: "Irish Tune from County Derry – P Grainger"
                     }
@@ -98,7 +98,7 @@ angular.module('mentorApp.controllers.about', [])
                       name: "An Outdoor Overture – A Copland",
                   },{
                       name: "October – E Whitacre",
-                      link: "https://www.dropbox.com/s/blu0z1w1z25ufi1/October_Whitacre_31OCT2012.mp3?dl=0",
+                      link: "audio/October_Whitacre_31OCT2012.mp3",
                   },{
                       name: "O Magnum Mysterium – Lauridsen",
                   }
@@ -109,16 +109,16 @@ angular.module('mentorApp.controllers.about', [])
       $scope.soloistRep = [
           {
               name: "They Can’t Take That Away From Me – G Gershwin (Millicent Scarlett, soprano)",
-              linky: "https://www.dropbox.com/s/xbpp5q2ezhpz8x4/TheyCantTakeThatAwayFromMe_Gershwin%28MillicentScarlett%2CSoprano%29_19JUN2013.mp3?dl=0",
+              linky: "audio/TheyCantTakeThatAwayFromMe_Gershwin%28MillicentScarlett%2CSoprano%29_19JUN2013.mp3",
           },{
               name: "Take The A Train – B Strayhorn (Chris Vadala, saxophone)",
-              link: "https://www.dropbox.com/s/727v2y5t77o1744/TakeTheATrain_Strayhorn%28ChrisVadala%2CSaxophone%29_24June2011.mp3?dl=0",
+              link: "audio/TakeTheATrain_Strayhorn%28ChrisVadala%2CSaxophone%29_24June2011.mp3",
           },{
               name: "Meditation from “Thaïs” – Massenet (Daniel Sender, violin)",
-              link: "https://www.dropbox.com/s/2ro467ocgvl9347/MeditationfromThais_Massenet%28DanielSender%2CViolin%29_09DEC2011.mp3?dl=0",
+              link: "audio/MeditationfromThais_Massenet%28DanielSender%2CViolin%29_09DEC2011.mp3",
           },{
               name: "Variations on Napoli – Bellstedt (Jermaine Fryer, euphonium)",
-              link: "https://www.dropbox.com/s/xhywf3br5k5xn9c/VariationsOnNapoli_Bellstedt%28JermaineFryer%2CEuphonium%29.mp3?dl=0",
+              link: "audio/VariationsOnNapoli_Bellstedt%28JermaineFryer%2CEuphonium%29.mp3",
           }
       ];
 
