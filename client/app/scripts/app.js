222c3081b1a4f58a7ea028150db931d9e6b4d9fc
Bob Schmertz <rschmertz@yahoo.com>
2021-10-17 23:15:12 -0400

    Update dues payment options

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -23,7 +23,7 @@ angular
   .config(function($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html?date=20210124',
+        templateUrl: 'views/main.html?date=20211017',
         controller: 'MainCtrl',
         name: 'main'
       })
