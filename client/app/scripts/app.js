03a0ab09b2e26b8ddb13c518ca8687d86f3860a6
Bob Schmertz <rschmertz@yahoo.com>
2024-10-12 11:41:27 -0400

    Changes to make sure previous changes get loaded

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -60,7 +60,7 @@ angular
         templateUrl: 'views/press.html',
       })
       .when('/events/', {
-        templateUrl: 'views/events.html?date-20240924',
+        templateUrl: 'views/events.html?date=20241012',
       })
       .when('/blog2/', {
         templateUrl: 'views/blog2.html',
